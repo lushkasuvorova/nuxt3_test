c3985fc90f553afa29b70a7abed9d622c98e08b4
lushkasuvorova <lushkasuvorova0@gmail.com>
2023-12-27 19:15:45 +0300

    1

---
+++ pages/[... slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+      <ContentDoc v-slot="{ doc }">
+        <article>
+          <h1>{{ doc.title }}</h1>
+          <h2>{{ doc.description }}</h2>
+          <h2>{{ doc.date }}</h2>
+          <ContentRenderer :value="doc" />
+        </article>
+      </ContentDoc>
+  </main>
+  comments
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
+const query: QueryBuilderParams = { path: '/' }
+</script>
+<style>
+a {
+  text-decoration: none;
+}
+
+.wrapper {
+  display: flex;
+}
+
+.block {
+  display: inline-block;
+  margin: 2px;
+}
+</style>
+<template>
+  <div>
+    Все посты<br>
+  </div>
+  <ContentList :query="query" v-slot="{ list }">
+    <div v-for="article in list" :key="article._path">
+      <a :href="article._path">
+        <div class="wrapper">
+          <div class="block">
+            <img :src="article.img" class="img-fluid" height="100px" />
+          </div>
+          <div class="block">
+            <h2>{{ article.title }}</h2>
+            <p>{{ article.description }}</p>
+          </div>
+        </div>
+      </a>
+    </div>
+  </ContentList>
+</template>
